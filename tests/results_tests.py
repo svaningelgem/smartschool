6238d2d184e393b10f19125fd1af556befa96c24
Steven Van Ingelgem <steven@vaningelgem.be>
2024-07-05 08:06:22 +0200

    "Fix" download errors to show a nicer exception. (#4)

--- tests/results_tests.py
+++ tests/results_tests.py
@@ -1,4 +1,6 @@
-from smartschool import ResultDetail, Results
+import pytest
+from requests_mock import ANY
+from smartschool import DownloadError, ResultDetail, Results
 
 
 def test_results_normal_flow(mocker):
@@ -19,3 +21,29 @@ def test_result_detail_normal_flow():
     assert sut.name == "Repetitie hoofdstuk 1"
     assert sut.graphic.achieved_points == 13.5
     assert sut.details.teachers[0].name.startingWithFirstName == "Gert Segers"
+
+
+def test_result_detail_with_empty_return_value():
+    with pytest.raises(DownloadError):
+        ResultDetail(result_id="empty_return_value").get()
+
+
+def test_result_detail_with_faulty_session_get(requests_mock):
+    requests_mock.register_uri(ANY, ANY, status_code=404)
+
+    with pytest.raises(DownloadError):
+        ResultDetail(result_id="abc_normal_123").get()
+
+
+def test_result_with_empty_return_value(requests_mock):
+    requests_mock.register_uri(ANY, ANY, text="")
+
+    with pytest.raises(DownloadError):
+        next(iter(Results()))
+
+
+def test_result_with_faulty_session_get(requests_mock):
+    requests_mock.register_uri(ANY, ANY, status_code=404)
+
+    with pytest.raises(DownloadError):
+        next(iter(Results()))
