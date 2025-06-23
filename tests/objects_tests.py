c8cc72308685b965bb9a05dccfc45828be4c3334
Steven Van Ingelgem <steven@vaningelgem.be>
2025-06-23 13:00:29 +0200

    move the time a little bit to account for DSH ?

--- tests/objects_tests.py
+++ tests/objects_tests.py
@@ -6,21 +6,21 @@ import time_machine
 from smartschool.objects import convert_to_date, convert_to_datetime
 
 
-@time_machine.travel("2023-09-01 01:02:03+02:00", tick=False)
+@time_machine.travel("2023-09-01 10:02:03+02:00", tick=False)
 def test_convert_to_datetime() -> None:
-    expected = datetime(2023, 9, 1, 1, 2, 3, tzinfo=timezone(timedelta(hours=2)))
+    expected = datetime(2023, 9, 1, 10, 2, 3, tzinfo=timezone(timedelta(hours=2)))
 
-    assert convert_to_datetime("2023-09-01T01:02:03+02:00") == expected
+    assert convert_to_datetime("2023-09-01T10:02:03+02:00") == expected
     assert convert_to_datetime(expected) == expected
     assert convert_to_datetime(None) == expected
 
     with pytest.raises(ValueError, match="No timezone information found in this date"):
         convert_to_datetime(expected.replace(tzinfo=None))
 
-    assert convert_to_datetime("2023-09-01 01:02") == expected.replace(second=0, tzinfo=None)
+    assert convert_to_datetime("2023-09-01 10:02") == expected.replace(second=0, tzinfo=None)
 
 
-@time_machine.travel("2023-09-01 01:02:03+02:00", tick=False)
+@time_machine.travel("2023-09-01 10:02:03+02:00", tick=False)
 def test_convert_to_date() -> None:
     expected = date(2023, 9, 1)
 
