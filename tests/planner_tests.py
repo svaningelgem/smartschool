540a51f9c92e37492582076c0adc3fa8ed84b66a
Steven Van Ingelgem <steven@vaningelgem.be>
2025-06-23 04:25:32 +0200

    Correcting failing test

--- tests/planner_tests.py
+++ tests/planner_tests.py
@@ -6,6 +6,6 @@ def test_periods_happy_flow():
 
     assert len(sut) == 2
 
-    assert sut[0].courses[0].name == "Frans"
+    assert sut[0].courses[0].name == "Duits"
     assert sut[0].plannedElementType == "planned-assignments"
     assert not sut[0].unconfirmed
