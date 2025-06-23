76d525ecbc0d7c0a97d033fe7162e2588c50e124
Steven Van Ingelgem <steven@vaningelgem.be>
2025-06-23 04:19:09 +0200

    Correcting failing test

--- tests/planner_tests.py
+++ tests/planner_tests.py
@@ -4,8 +4,8 @@ from smartschool.planner import PlannedElements
 def test_periods_happy_flow():
     sut = list(PlannedElements())
 
-    assert len(sut) == 25
+    assert len(sut) == 2
 
-    assert sut[0].courses[0].name == "Godsdienst"
-    assert sut[0].plannedElementType == "planned-placeholders"
+    assert sut[0].courses[0].name == "Frans"
+    assert sut[0].plannedElementType == "planned-assignments"
     assert not sut[0].unconfirmed
